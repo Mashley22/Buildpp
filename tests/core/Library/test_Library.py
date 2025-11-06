d2da496218c8f80e697fee7badd038264acf3277
Mashley22 <ajkhsu22@gmail.com>
2025-11-06 22:19:05 +0000

    Added some basic tests for test_Library

---
+++ tests/core/Library/test_Library.py
@@ -0,0 +1,89 @@
+from buildpp.buildpp import new_Library
+from buildpp.library import (
+    SourcesList,
+    IncludeDirsList,
+    CompileDefinitionsList,
+    CompilerFlagsList,
+    Version,
+    LibType
+)
+
+import pytest
+
+
+class Test_Library:
+
+    def test_name(self):
+
+        libName = "Insert funny name here"
+        test = new_Library(libName)
+
+        assert test.name == libName
+
+    class Test_libType:
+        def test_basic(self):
+
+            test = new_Library("type")
+
+            assert test.libType is not None  # the lib type needs to be assigned even at default
+
+            test.setLibType(LibType.SHARED)
+
+            assert test.libType == LibType.SHARED
+
+            test.setLibType(LibType.STATIC)
+
+            assert test.libType == LibType.STATIC
+
+        def test_TypeError(self):
+
+            test = new_Library("type")
+
+            errVals = ["STATIC", "SHARED", [], 6]
+
+            for ele in errVals:
+                with pytest.raises(AssertionError):
+                    test.setLibType(ele)
+
+# ____________________SOURCES____________________
+    class Test_Sources:
+        def test_basic(self):
+
+            test = new_Library("sources")
+
+            sources = SourcesList(["a", "b", "c"])
+
+            test.sources = sources
+
+            assert test.sources.sources == sources.sources
+
+        def test_TypeError(self):
+
+            test = new_Library("sources")
+
+            errVals = [6, [], "foo"]
+
+            for ele in errVals:
+                with pytest.raises(AssertionError):
+                    test.sources = ele
+
+# ____________________VERSION____________________
+    class Test_Version:
+        def test_basic(self):
+
+            test = new_Library("version")
+            testVersion = Version("0.0.6.4")
+
+            test.version = testVersion
+
+            assert test.version == testVersion
+
+        def test_TypeError(self):
+
+            test = new_Library("version")
+
+            errVals = [2930.0, [], "version"]
+
+            for ele in errVals:
+                with pytest.raises(AssertionError):
+                    test.version = ele
